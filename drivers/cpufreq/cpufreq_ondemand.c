7267d61befe7ef12dc674e7a3cdb7ef730884fb5
flar2 <asegaert@gmail.com>
2014-08-07 20:41:41 -0400

    elementalx gov:  only two cores on this phone

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -473,7 +473,7 @@ static ssize_t store_two_phase_freq(struct kobject *a, struct attribute *b,
 	return count;
 }
 
-static int input_event_min_freq_array[NR_CPUS] = {1134000, 1134000, 1134000, 1134000} ;
+static int input_event_min_freq_array[NR_CPUS] = {1134000, 1134000} ;
 
 static ssize_t show_input_event_min_freq
 (struct kobject *kobj, struct attribute *attr, char *buf)
