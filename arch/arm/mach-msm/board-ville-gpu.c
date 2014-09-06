88ef0623f04126e1a8ed45945e4d608dcec81ff2
flar2 <asegaert@gmail.com>
2014-09-05 22:56:28 -0400

    GPU OC: replace some changes undone by moving to separate GPU file

--- arch/arm/mach-msm/board-ville-gpu.c
+++ arch/arm/mach-msm/board-ville-gpu.c
@@ -295,7 +295,7 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 400000000,
+			.gpu_freq = 480000000,
 			.bus_freq = 3,
 			.io_fraction = 0,
 		},
@@ -371,7 +371,7 @@ static struct kgsl_device_iommu_data kgsl_2d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 266000000,
 			.bus_freq = 2,
 		},
 		{
@@ -438,7 +438,7 @@ static struct resource kgsl_2d1_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 266000000,
 			.bus_freq = 2,
 		},
 		{
